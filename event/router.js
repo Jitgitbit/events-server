ed12d8ff0766265a932b0043a389d76c8ed861f2
Jitgitbit <rowowolf@gmail.com>
2020-02-06 14:05:43 +0100

    all done

--- event/router.js
+++ event/router.js
@@ -3,29 +3,46 @@ const { Router } = require('express')
 const router = new Router()
 const db = require(`../db`);
 
-// router.get('/team', (req, res, next) => {
-//   Team.findAll()
-//     .then(teams => {
-//       res.send(teams);
-//     })
-//     .catch(err => next(err))
-// });
+router.get('/event', (req, res, next) => {
+  Event.findAll()
+    .then(events => {
+      res.send(events);
+    })
+    .catch(err => next(err))
+});
 
-// router.get('/team/:id', (req, res, next) => {
-//   Team.findByPk(req.params.id) // beware: if you want to define a const with req.params.id you have to parseInt(), because Url returns a string !!
-//     .then(team => {
-//       res.json(team); //send for strings, json for data. it works both but it's better practice!
-//     })
-//     .catch(err => next(err))
-// });
+router.get('/event/:id', (req, res, next) => {
+  Event.findByPk(parseInt(req.params.id)) // beware: if you want to define a const with req.params.id you have to parseInt(), because Url returns a string !!
+    .then(event => {                      // above, I just did it here, although not required as it is not defined in a variable const
+      res.json(event); //send for strings, json for data. it works both but it's better practice!
+    })
+    .catch(err => next(err))
+});
 
-// router.post(`/team`, (req, res, next) => {
-//   console.log(req.body)
-//   Team.create(req.body)
-//     .then(team => {
-//       res.send(team);
-//     })
-//     .catch(err => next(err))
-// })
+router.post(`/event`, (req, res, next) => {
+  console.log(req.body)
+  Event.create(req.body)
+    .then(event => {
+      res.send(event);
+    })
+    .catch(err => next(err))
+})
 
-module.exports = router
+router.put(
+  '/event/:id',
+  (request, response, next) => Event
+    .findByPk(request.params.id)
+    .then(event => event.update(request.body))
+    .then(event => response.send(event))
+    .catch(next)
+)
+
+router.delete(
+  '/event/:id',
+  (request, response, next) => Event
+    .destroy({ where: { id: request.params.id }})
+    .then(number => response.send({ number }))
+    .catch(next)
+)
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,37 +1,24 @@
 const express = require('express');
 const db = require(`./db`)
-const Event = require(`./event/model`);
 const bodyParser = require('body-parser');
 const cors = require(`cors`);
-const Event = require(`./event/router`);
+const Event = require(`./event/model`);
+const router = require(`./event/router`);
 
 
 const app = express();
+
 const corsMiddleware = cors()
 app.use(corsMiddleware)
+
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
+
 app.use(router)
 
 
 const port = process.env.PORT || 4000;
 
-
-// app.get('/doctor', (req, res, next)=>{
-//   console.log('getting doctor');
-//   const name = req.query.name || 'Who'; // .QUERY! not .body
-//   const doctor = 'Dr. ' + name;
-//   res.send(doctor);
-//   next();
-// })
-// app.get('/appointment', (req, res, next)=>{
-//   console.log('getting appointment');
-//   const doctor = req.query.doctor || 'Eggman';
-//   const patient = req.query.patient || 'zero';
-//   const description = `Dr. ${doctor} and patient ${patient}`;
-//   res.send(description);
-//   next();
-// })
 
 app.listen(port, () => {
     console.log(`Listening on :${port}`) // USE THIS NOTATION FOR THE PORT !! 
