5033115ee94a1ad157cc5610268f463cad611c27
Jitgitbit <rowowolf@gmail.com>
2020-02-07 12:16:54 +0100

    not event, but events

--- event/router.js
+++ event/router.js
@@ -3,7 +3,7 @@ const { Router } = require('express')
 const router = new Router()
 const db = require(`../db`);
 
-router.get('/event', (req, res, next) => {
+router.get('/events', (req, res, next) => {
   Event.findAll()
     .then(events => {
       res.send(events);
@@ -11,15 +11,15 @@ router.get('/event', (req, res, next) => {
     .catch(err => next(err))
 });
 
-router.get('/event/:id', (req, res, next) => {
-  Event.findByPk(parseInt(req.params.id)) // beware: if you want to define a const with req.params.id you have to parseInt(), because Url returns a string !!
-    .then(event => {                      // above, I just did it here, although not required as it is not defined in a variable const
+router.get('/events/:id', (req, res, next) => {
+  Event.findByPk(req.params.id) // beware: if you want to define a const with req.params.id you have to parseInt(), because Url returns a string !!
+    .then(event => {                      
       res.json(event); //send for strings, json for data. it works both but it's better practice!
     })
     .catch(err => next(err))
 });
 
-router.post(`/event`, (req, res, next) => {
+router.post(`/events`, (req, res, next) => {
   console.log(req.body)
   Event.create(req.body)
     .then(event => {
@@ -29,7 +29,7 @@ router.post(`/event`, (req, res, next) => {
 })
 
 router.put(
-  '/event/:id',
+  '/events/:id',
   (request, response, next) => Event
     .findByPk(request.params.id)
     .then(event => event.update(request.body))
@@ -38,7 +38,7 @@ router.put(
 )
 
 router.delete(
-  '/event/:id',
+  '/events/:id',
   (request, response, next) => Event
     .destroy({ where: { id: request.params.id }})
     .then(number => response.send({ number }))
